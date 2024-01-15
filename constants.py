e5250b904fae9e882708ea9c3d65d20e7bf95236
gizembrasser <gizembrasser101@gmail.com>
2024-01-15 16:48:21 +0100

    add: DATA_DIR and EMAIL_FILE

--- constants.py
+++ constants.py
@@ -9,4 +9,7 @@ EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
 PASSWORD = os.getenv("PASSWORD")
 
 # Declare path to file directory
-FILE_DIR = os.path.join
+DATA_DIR = os.path.join(os.getcwd(), "data")
+
+# Declare the path to the data files
+EMAIL_FILE = os.path.join(DATA_DIR, "emails.txt")
--- data/constants.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-EMAIL_SERVER = os.getenv("EMAIL_SERVER")
-EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
-PASSWORD = os.getenv("PASSWORD")
